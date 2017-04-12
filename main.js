c45f57f349950d455bdc95f3bb8920700da7907a
IvanGenesoto <igenesoto@gmail.com>
2017-04-11 19:49:28 -0700

    Made camera follow user. #3

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-var $car = document.createElement('h1')
-$car.textContent = 'o=o'
-$car.setAttribute('id', 'car')
-document.body.appendChild($car)
-
-var $truck = document.createElement('h1')
-$truck.textContent = '=o=[]o'
-$truck.setAttribute('id', 'truck')
-document.body.appendChild($truck)
-
-var $bus = document.createElement('h1')
-$bus.textContent = '[o===o]'
-$bus.setAttribute('id', 'bus')
-document.body.appendChild($bus)
-
-var $walk = document.createElement('h1')
-$walk.textContent = 'i'
-$walk.setAttribute('id', 'walk')
-document.body.appendChild($walk)
-
-var $bugatti = document.createElement('h1')
-$bugatti.textContent = 'O==o'
-$bugatti.setAttribute('id', 'bugatti')
-document.body.appendChild($bugatti)
+var accelerating = false
+var decelerating = false
+var container = document.querySelector('#container')
 
 class Vehicle {
-  constructor(location, direction, speed, marker) {
+  constructor(name, ascii, location, direction, speed) {
     this.direction = direction
     this.speed = speed
-    this.marker = marker
     this.location = location
+    this.width = ascii.east.length * 20
+    this.markerEast = document.createElement('h1')
+    this.markerEast.textContent = ascii.east
+    var idEast = name + '-east'
+    this.markerEast.setAttribute('id', idEast)
+    container.appendChild(this.markerEast)
+    this.markerEast.classList.add('vehicle')
+    this.markerWest = document.createElement('h1')
+    this.markerWest.textContent = ascii.west
+    var idWest = name + '-west'
+    this.markerWest.setAttribute('id', idWest)
+    this.markerWest.classList.add('vehicle')
+    container.appendChild(this.markerWest)
+    if (direction === 'east') {
+      this.markerWest.classList.add('hidden')
+    }
+    else this.markerEast.classList.add('hidden')
+    this.markerEast.style.left = this.location.left + 'px'
+    this.markerWest.style.left = this.location.left + 'px'
+    this.markerEast.style.top = this.location.top + 'px'
+    this.markerWest.style.top = this.location.top + 'px'
   }
   getDirection() {
     return this.direction
@@ -43,38 +42,90 @@ class Vehicle {
     this.speed = newSpeed
   }
   accelerate() {
-    if (this.marker === $walk && this.speed > 2) return
-    else if (this.marker === $bus && this.speed > 4) return
-    else if (this.marker === $truck && this.speed > 7) return
+    if (this.markerEast.getAttribute('id') === 'walk-east' && this.speed > 2) return
+    else if (this.markerEast.getAttribute('id') === 'bus-east' && this.speed > 4) return
+    else if (this.markerEast.getAttribute('id') === 'truck-east' && this.speed > 7) return
+    else if (this.markerEast.getAttribute('id') === 'car-east' && this.speed > 11) return
     else this.speed += 1
   }
   decelerate() {
-    if (this.speed >= 1) {
-      this.speed -= 1
+    if (this.markerEast.getAttribute('id') === 'walk-east' && this.speed > 0) this.speed = 0
+    else if (this.markerEast.getAttribute('id') === 'bus-east' && this.speed > 0) this.speed -= 1
+    else if (this.markerEast.getAttribute('id') === 'truck-east') {
+      if (this.speed === 0 || this.speed === 1) this.speed = 0
+      else if (this.speed === 2) this.speed = 1
+      else this.speed -= 2
+    }
+    else if (this.markerEast.getAttribute('id') === 'car-east') {
+      if (this.speed > -1 && this.speed < 3) this.speed = 0
+      else if (this.speed === 3) this.speed = 1
+      else this.speed -= 3
+    }
+    else if (this.markerEast.getAttribute('id') === 'bugatti-east') {
+      if (this.speed > -1 && this.speed < 7) this.speed = 0
+      else if (this.speed > 6 && this.speed < 30) this.speed -= 7
+      else this.speed -= 14
     }
   }
-  updateLocation() {
+  updateLocation(filteredVehicles) {
     if (this.direction === 'north') {
-      this.location.top -= this.speed
-      this.marker.style.top = this.location.top + 'px'
+      filteredVehicles.forEach(function (vehicle) {
+        vehicle.location.top += currentVehicle.speed
+        vehicle.markerEast.style.top = vehicle.location.top + 'px'
+        vehicle.markerWest.style.top = vehicle.location.top + 'px'
+      })
     }
     if (this.direction === 'south') {
-      this.location.top += this.speed
-      this.marker.style.top = this.location.top + 'px'
+      filteredVehicles.forEach(function (vehicle) {
+        vehicle.location.top -= currentVehicle.speed
+        vehicle.markerEast.style.top = vehicle.location.top + 'px'
+        vehicle.markerWest.style.top = vehicle.location.top + 'px'
+      })
     }
     if (this.direction === 'east') {
-      this.location.left += this.speed
-      this.marker.style.left = this.location.left + 'px'
+      this.markerEast.classList.remove('hidden')
+      this.markerWest.classList.add('hidden')
+      filteredVehicles.forEach(function (vehicle) {
+        vehicle.location.left -= currentVehicle.speed
+        vehicle.markerEast.style.left = vehicle.location.left + 'px'
+        vehicle.markerWest.style.left = vehicle.location.left + 'px'
+      })
     }
     if (this.direction === 'west') {
-      this.location.left -= this.speed
-      this.marker.style.left = this.location.left + 'px'
+      this.markerEast.classList.add('hidden')
+      this.markerWest.classList.remove('hidden')
+      filteredVehicles.forEach(function (vehicle) {
+        vehicle.location.left += currentVehicle.speed
+        vehicle.markerEast.style.left = vehicle.location.left + 'px'
+        vehicle.markerWest.style.left = vehicle.location.left + 'px'
+      })
     }
   }
-  start() {
-    this.marker.style.left = this.location.left + 'px'
-    this.marker.style.top = this.location.top + 'px'
-  }
+}
+
+function move(filteredVehicles) {
+  filteredVehicles.forEach(vehicle => {
+    if (vehicle.direction === 'north') {
+      vehicle.location.top -= vehicle.speed
+      vehicle.markerEast.style.top = vehicle.location.top + 'px'
+      vehicle.markerWest.style.top = vehicle.location.top + 'px'
+    }
+    if (vehicle.direction === 'south') {
+      vehicle.location.top += vehicle.speed
+      vehicle.markerEast.style.top = vehicle.location.top + 'px'
+      vehicle.markerWest.style.top = vehicle.location.top + 'px'
+    }
+    if (vehicle.direction === 'east') {
+      vehicle.location.left += vehicle.speed
+      vehicle.markerEast.style.left = vehicle.location.left + 'px'
+      vehicle.markerWest.style.left = vehicle.location.left + 'px'
+    }
+    if (vehicle.direction === 'west') {
+      vehicle.location.left -= vehicle.speed
+      vehicle.markerEast.style.left = vehicle.location.left + 'px'
+      vehicle.markerWest.style.left = vehicle.location.left + 'px'
+    }
+  })
 }
 
 class SuperCar extends Vehicle {
@@ -83,45 +134,91 @@ class SuperCar extends Vehicle {
   }
 }
 
-var car = new Vehicle({left: 250, top: 400}, 'east', 0, $car)
-var truck = new Vehicle({left: 500, top: 500}, 'east', 0, $truck)
-var bus = new Vehicle({left: 400, top: 200}, 'east', 0, $bus)
-var walk = new Vehicle({left: 350, top: 350}, 'east', 0, $walk)
-var bugatti = new SuperCar({left: 100, top: 100}, 'east', 0, $bugatti)
+function filterVehicles() {
+  var filteredVehicles = vehicles.filter(function (vehicle) {
+    return vehicle !== currentVehicle
+  })
+  return filteredVehicles
+}
 
-car.start()
-truck.start()
-bus.start()
-walk.start()
-bugatti.start()
+var car = new Vehicle('car', {east: 'o=o.', west: '.o=o'}, {left: 250, top: 400}, 'east', 3)
+var truck = new Vehicle('truck', {east: '=o=[]o', west: 'o[]=o='}, {left: 500, top: 500}, 'south', 2)
+var bus = new Vehicle('bus', {east: '[o===o].', west: '.[o===o]'}, {left: 400, top: 200}, 'west', 1)
+var walk = new Vehicle('walk', {east: 'i', west: 'i'}, {left: 345, top: 327}, 'east', 0)
+var bugatti = new SuperCar('bugatti', {east: '\'O=o.', west: '.o=O\''}, {left: 100, top: 100}, 'east', 5)
+
+var vehicles = [car, bus, truck, bugatti]
 
 var currentVehicle = walk
 
-setInterval(function () {
-  function hijack(vehicle, vehicleWidth) {
+function hijack(filteredVehicles) {
+  var hijacked = false
+  vehicles.forEach(function (vehicle) {
     if (
-      currentVehicle === walk && (
-        walk.location.left > vehicle.location.left + 15 &&
-        walk.location.left < vehicle.location.left + vehicleWidth &&
-        walk.location.top > vehicle.location.top - 15 &&
-        walk.location.top < vehicle.location.top + 15
-      )
+      currentVehicle === walk &&
+      walk.location.left > vehicle.location.left + 15 &&
+      walk.location.left < vehicle.location.left + vehicle.width - 15 &&
+      walk.location.top > vehicle.location.top - 15 &&
+      walk.location.top < vehicle.location.top + 15
     ) {
+      hijacked = true
       currentVehicle = vehicle
       walk.setSpeed(0)
-      $walk.classList.add('hidden')
+      var $walkEast = document.querySelector('#walk-east')
+      var $walkWest = document.querySelector('#walk-west')
+      $walkEast.classList.add('hidden')
+      $walkWest.classList.add('hidden')
+      var previousTop = currentVehicle.location.top
+      var previousLeft = currentVehicle.location.left
+      currentVehicle.location.top = 327
+      currentVehicle.location.left = 345 - currentVehicle.width / 2
+      currentVehicle.markerEast.style.top = currentVehicle.location.top + 'px'
+      currentVehicle.markerWest.style.top = currentVehicle.location.top + 'px'
+      currentVehicle.markerEast.style.left = currentVehicle.location.left + 'px'
+      currentVehicle.markerWest.style.left = currentVehicle.location.left + 'px'
+      var filteredVehicles = filterVehicles()
+      filteredVehicles.forEach(function (otherVehicle) {
+        if (previousTop > vehicle.location.top) {
+          otherVehicle.location.top -= previousTop - vehicle.location.top
+          otherVehicle.markerEast.style.top = otherVehicle.location.top + 'px'
+          otherVehicle.markerWest.style.top = otherVehicle.location.top + 'px'
+        }
+        if (previousTop < vehicle.location.top) {
+          otherVehicle.location.top += vehicle.location.top - previousTop
+          otherVehicle.markerEast.style.top = otherVehicle.location.top + 'px'
+          otherVehicle.markerWest.style.top = otherVehicle.location.top + 'px'
+        }
+        if (previousLeft > vehicle.location.left) {
+          otherVehicle.location.left -= previousLeft - vehicle.location.left
+          otherVehicle.markerEast.style.left = otherVehicle.location.left + 'px'
+          otherVehicle.markerWest.style.left = otherVehicle.location.left + 'px'
+        }
+        if (previousLeft < vehicle.location.left) {
+          otherVehicle.location.left += vehicle.location.left - previousLeft
+          otherVehicle.markerEast.style.left = otherVehicle.location.left + 'px'
+          otherVehicle.markerWest.style.left = otherVehicle.location.left + 'px'
+        }
+      })
     }
+  })
+  return hijacked
+}
+
+setInterval(function () {
+  var hijacked = false
+  hijacked = hijack()
+  if (hijacked === false) {
+    var filteredVehicles = filterVehicles()
+    currentVehicle.updateLocation(filteredVehicles)
   }
-  car.updateLocation()
-  truck.updateLocation()
-  bus.updateLocation()
-  walk.updateLocation()
-  bugatti.updateLocation()
-  hijack(car, 40)
-  hijack(truck, 80)
-  hijack(bus, 120)
-  hijack(bugatti, 80)
+  filteredVehicles = filterVehicles()
+  move(filteredVehicles)
 }, 42)
+
+setInterval(function () {
+  if (accelerating) currentVehicle.accelerate()
+  if (decelerating) currentVehicle.decelerate()
+}, 500)
 
 document.body.addEventListener('keydown', function (event) {
   function controlVehicle() {
@@ -138,24 +235,61 @@ document.body.addEventListener('keydown', function (event) {
       currentVehicle.setDirection('west')
     }
     if (event.key === '=' || event.key === 'a') {
-      currentVehicle.accelerate()
+      accelerating = true
     }
     if (event.key === '-' || event.key === 'd') {
-      currentVehicle.decelerate()
+      decelerating = true
     }
   }
-  function getOut(vehicle, vehicleWidth) {
+  function getOut(vehicle, vehicleWidth, filteredVehicles) {
     if (
       currentVehicle === vehicle &&
-      vehicle.getSpeed() === 0 &&
+      vehicle.speed === 0 &&
       event.key === ' '
     ) {
-      walk.location.left = currentVehicle.location.left + vehicleWidth / 2
-      walk.location.top = currentVehicle.location.top + 16
-      walk.direction = currentVehicle.direction
-      walk.start()
+      walk.location.top = 327
+      walk.location.left = 345
+      walk.direction = 'south'
+      var previousTop = vehicle.location.top
+      var previousLeft = vehicle.location.left
+      vehicle.location.top -= 16
+      filteredVehicles.forEach(function (otherVehicle) {
+        if (previousTop > vehicle.location.top) {
+          otherVehicle.location.top -= previousTop - vehicle.location.top
+          otherVehicle.markerEast.style.top = otherVehicle.location.top + 'px'
+          otherVehicle.markerWest.style.top = otherVehicle.location.top + 'px'
+        }
+        if (previousTop < vehicle.location.top) {
+          otherVehicle.location.top += vehicle.location.top - previousTop
+          otherVehicle.markerEast.style.top = otherVehicle.location.top + 'px'
+          otherVehicle.markerWest.style.top = otherVehicle.location.top + 'px'
+        }
+        if (previousLeft > vehicle.location.left) {
+          otherVehicle.location.left -= previousLeft - vehicle.location.left
+          otherVehicle.markerEast.style.left = otherVehicle.location.left + 'px'
+          otherVehicle.markerWest.style.left = otherVehicle.location.top + 'px'
+        }
+        if (previousLeft < vehicle.location.left) {
+          otherVehicle.location.left += vehicle.location.left - previousLeft
+          otherVehicle.markerEast.style.left = otherVehicle.location.left + 'px'
+          otherVehicle.markerWest.style.left = otherVehicle.location.top + 'px'
+        }
+      })
+      vehicle.markerEast.style.top = vehicle.location.top + 'px'
+      vehicle.markerWest.style.top = vehicle.location.top + 'px'
+      vehicle.markerEast.style.left = vehicle.location.left + 'px'
+      vehicle.markerWest.style.left = vehicle.location.left + 'px'
+      walk.markerEast.style.left = walk.location.left + 'px'
+      walk.markerWest.style.left = walk.location.left + 'px'
+      var $walkEast = document.querySelector('#walk-east')
+      var $walkWest = document.querySelector('#walk-west')
+      if (walk.direction === 'east') {
+        $walkEast.classList.remove('hidden')
+      }
+      else {
+        $walkWest.classList.remove('hidden')
+      }
       currentVehicle = walk
-      $walk.classList.remove('hidden')
     }
   }
   if (
@@ -167,9 +301,15 @@ document.body.addEventListener('keydown', function (event) {
   ) {
     event.preventDefault()
   }
+  var filteredVehicles = filterVehicles()
+  getOut(car, 40, filteredVehicles)
+  getOut(truck, 80, filteredVehicles)
+  getOut(bus, 140, filteredVehicles)
+  getOut(bugatti, 80, filteredVehicles)
   controlVehicle()
-  getOut(car, 40)
-  getOut(truck, 80)
-  getOut(bus, 120)
-  getOut(bugatti, 80)
+})
+
+document.body.addEventListener('keyup', function (event) {
+  if (event.key === 'a') accelerating = false
+  if (event.key === 'd') decelerating = false
 })
