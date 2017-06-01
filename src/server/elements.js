c292cf433fb62139c441079b7129233db7c31ec9
IvanGenesoto <igenesoto@gmail.com>
2017-06-01 10:30:10 -0700

    Added ancillary functions to accommodate districts element type. #48

--- src/server/elements.js
+++ src/server/elements.js
@@ -1,10 +1,28 @@
-const _zones = {
-  status: '',
-  established: undefined,
+const _districts = {
+  status: [''],
+  established: [0],
   rooms: [[0]],
   characters: [[0]],
   vehicles: [[0]],
-  unwelcomes: [[0]]
+  unwelcomes: [[0]],
+  scenery: [
+    [ // stratum 0
+      [ // layer 0
+        [ // row 0
+          'images/background/far/above-top.png' // column 0 scenery
+        ]
+      ]
+    ]
+  ],
+  collision: [
+    [ // stratum 0
+      [ // layer 0
+        [ // row 0
+          [0] // column 0 collision
+        ]
+      ]
+    ]
+  ]
 }
 
 const _players = {
@@ -104,20 +122,22 @@ const _rooms = {
   inventory: [[0]]
 }
 
+const standinArray = []
+
 function Elements(_elements) {
 
   if (typeof _elements === 'string') {
     switch (_elements) {
+      case '_zones': _elements = _districts; break
+      case '_players': _elements = _players; break
       case '_characters': _elements = _characters; break
       case '_vehicles': _elements = _vehicles; break
       case '_rooms': _elements = _rooms; break
-      case '_players': _elements = _players; break
-      case '_districts': _elements = _zones; break
       default: throw console.log('Element type not found')
     }
   }
 
-  function createCharacter(name) {
+  function createElement(name) {
     const index = _elements.status.length
     const attributes = Object.values(_elements)
     attributes.forEach(attribute => {
@@ -141,19 +161,36 @@ function Elements(_elements) {
     const standinArray = accessorPrototype.standinArray
     const attributeNames = Object.keys(_elements)
     attributeNames.forEach(attributeName => {
-      const attribute = _elements[attributeName]
-      const defaultValue = attribute[0]
-      if (Array.isArray(defaultValue)) {
-        const propertyDescriptor = createArrayPropertyDescriptor(attributeName, standinArray)
-        Object.defineProperty(accessorPrototype, attributeName, propertyDescriptor)
-      }
-      else if (typeof defaultValue !== 'object') {
-        const propertyDescriptor = createDefaultPropertyDescriptor(attributeName)
-        Object.defineProperty(accessorPrototype, attributeName, propertyDescriptor)
-      }
-      else throw console.log('Object or null found in default element')
+      accessorPrototype = defineProperty(accessorPrototype, standinArray, attributeName)
     })
     return accessorPrototype
+  }
+
+  function defineProperty(accessorPrototype, standinArray, attributeName) {
+    const attribute = _elements[attributeName]
+    const defaultValue = attribute[0]
+    if (Array.isArray(defaultValue)) {
+      return defineArrayProperty(accessorPrototype, standinArray, attributeName)
+    }
+    else if (typeof defaultValue !== 'object') {
+      return defineDefaultProperty(accessorPrototype, attributeName)
+    }
+    else throw console.log('Object or null found in default element')
+  }
+
+  function defineDefaultProperty(accessorPrototype, attributeName) {
+    const propertyDescriptor = createDefaultPropertyDescriptor(attributeName)
+    return Object.defineProperty(accessorPrototype, attributeName, propertyDescriptor)
+  }
+
+  function defineArrayProperty(accessorPrototype, standinArray, attributeName) {
+    const attribute = _elements[attributeName]
+    const defaultValue = attribute[0]
+    if (!Array.isArray(defaultValue[0])) {
+      var propertyDescriptor = createNestedArrayPropertyDescriptor(attributeName)
+    }
+    else propertyDescriptor = createArrayPropertyDescriptor(attributeName, standinArray)
+    return Object.defineProperty(accessorPrototype, attributeName, propertyDescriptor)
   }
 
   function createDefaultPropertyDescriptor(attributeName) {
@@ -180,6 +217,38 @@ function Elements(_elements) {
       },
       set: function(value) {
         return elements[attributeName](this.index, value)
+      }
+    }
+  }
+
+  function createNestedArrayPropertyDescriptor(attributeName) {
+    const attribute = _elements[attributeName]
+    const defaultValue = attribute[[[[0]]]]
+    if (Number.isInteger(defaultValue)) {
+      return createNestedIntegerPropertyDescriptor(attributeName)
+    }
+    else if (typeof defaultValue === 'string') {
+      return createNestedStringPropertyDescriptor(attributeName)
+    }
+    else throw console.log('Cannot create nested property descriptor of non-integer or -string')
+  }
+
+  function createNestedIntegerPropertyDescriptor(attributeName) {
+    return {
+      get: () => console.log('Use setter'),
+      set: function(value) {
+        return elements[attributeName](value)
+      }
+    }
+  }
+
+  function createNestedStringPropertyDescriptor(attributeName) {
+    return {
+      get: function() {
+        return console.log('Scenery getter not implemented')
+      },
+      set: function(value) {
+        return console.log('Scenery setter not implemented')
       }
     }
   }
@@ -216,7 +285,8 @@ function Elements(_elements) {
 
   function createArraySetter(attributeName, defaultValue) {
     const typeofDefaultValue = typeof defaultValue
-    if (Number.isInteger(defaultValue)) createIntegerArraySetter(attributeName)
+    if (Array.isArray(typeofDefaultValue)) createNestedArraySetter(attributeName)
+    else if (Number.isInteger(defaultValue)) createIntegerArraySetter(attributeName)
     else if (typeofDefaultValue !== 'object') createDefaultArraySetter(attributeName, typeofDefaultValue)
     else throw console.log('Non-integer found in default _elements.' + attributeName)
   }
@@ -229,8 +299,10 @@ function Elements(_elements) {
       if (typofValue === typeofDefaultValue) {
         if (value > 0) return push(value, array)
         else if (value < 0) return array.shift()
-        else return array.length
+        else throw console.log('Cannot push 0 to ' + attributeName)
       }
+      else if (value === 'length') return array.length
+      else if (value === 'clear') array.length = 0
       else throw console.log('elements[' + index + '].' + attributeName + ' must be a ' + typeofDefaultValue)
     }
   }
@@ -242,9 +314,22 @@ function Elements(_elements) {
       if (Number.isInteger(value)) {
         if (value > 0) return push(value, array)
         else if (value < 0) return remove(value, array)
-        else return array.length
+        else throw console.log('Cannot push 0 to ' + attributeName)
       }
+      else if (value === 'length') return array.length
+      else if (value === 'clear') array.length = 0
       else throw console.log('elements[' + index + '].' + attributeName + ' must be an integer')
+    }
+  }
+
+  function createNestedArraySetter(attributeName) {
+    const attribute = _elements[attributeName]
+    elements[attributeName] = function(joinedValue) {
+      if (joinedValue === 'clear') return clear(attributeName)
+      const [stratum, layer, row, collumn, value] = joinedValue.split('.')
+      const array = attribute[stratum][layer][row][collumn]
+      if (value) return set(array)
+      else return get(array)
     }
   }
 
@@ -265,11 +350,41 @@ function Elements(_elements) {
     }
   }
 
+  function get(array) {
+    standinArray.length = 0
+    array.forEach((value, index) => {
+      standinArray[index] = value
+    })
+    return standinArray
+  }
+
+  function set(value, array) {
+    if (Number.isInteger(value)) {
+      if (value > 0) return push(value, array)
+      else if (value < 0) return remove(value, array)
+      else throw console.log('Cannot push 0')
+    }
+    else if (value === 'length') return array.length
+    else if (value === 'clear') array.length = 0
+    else throw console.log('element must be an integer')
+  }
+
+  function clear(attributeName) {
+    const array = _elements[attributeName]
+    loopThrough(array)
+    function loopThrough(array) {
+      array.forEach(array => {
+        if (Array.isArray(array[0])) loopThrough(array)
+        else array.length = 0
+      })
+    }
+  }
+
   const elements = {
 
     length: _elements.length,
 
-    create: name => createAccessor(createCharacter(name))
+    create: name => createAccessor(createElement(name))
 
   }
 
