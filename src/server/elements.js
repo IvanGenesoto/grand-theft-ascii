4d9bcc8ca877f4a2b33852aaf77913d78b4afb1f
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2017-05-31 19:48:48 -0700

    Converted Characters module into Elements module in order to reuse code for all element types. #48

--- src/server/elements.js
+++ src/server/elements.js
@@ -1,4 +1,4 @@
-function Characters(_characters = {
+let _characters = {
   name: [''],
   status: [''],
   player: [0],
@@ -26,33 +26,37 @@ function Characters(_characters = {
   element: ['img'],
   elementID: [''],
   src: ['images/characters/man.png']
-}) {
+}
+
+function Elements(_elements) {
+
+  if (typeof _elements === 'string' && _elements === '_characters') _elements = _characters
 
   function createCharacter(name) {
-    const index = _characters.name.length
-    const attributes = Object.values(_characters)
+    const index = _elements.status.length
+    const attributes = Object.values(_elements)
     attributes.forEach(attribute => {
       const defaultValue = attribute[0]
       if (Array.isArray(defaultValue)) attribute[index] = []
       else if (typeof defaultValue !== 'object') attribute[index] = defaultValue
-      else throw console.log('Object or null found in default character')
+      else throw console.log('Object or null found in default element')
     })
     return index
   }
 
   function createAccessor(index) {
-    characters[index] = Object.create(accessorPrototype, {index: {value: index}})
-    Object.freeze(characters[index])
+    elements[index] = Object.create(accessorPrototype, {index: {value: index}})
+    Object.freeze(elements[index])
     return index
   }
 
-  function createAccessorPrototype(_characters) {
+  function createAccessorPrototype(_elements) {
     let accessorPrototype = Object.create(null)
     accessorPrototype.standinArray = []
     const standinArray = accessorPrototype.standinArray
-    const attributeNames = Object.keys(_characters)
+    const attributeNames = Object.keys(_elements)
     attributeNames.forEach(attributeName => {
-      const attribute = _characters[attributeName]
+      const attribute = _elements[attributeName]
       const defaultValue = attribute[0]
       if (Array.isArray(defaultValue)) {
         const propertyDescriptor = createArrayPropertyDescriptor(attributeName, standinArray)
@@ -62,19 +66,19 @@ function Characters(_characters = {
         const propertyDescriptor = createDefaultPropertyDescriptor(attributeName)
         Object.defineProperty(accessorPrototype, attributeName, propertyDescriptor)
       }
-      else throw console.log('Object or null found in default character')
+      else throw console.log('Object or null found in default element')
     })
     return accessorPrototype
   }
 
   function createDefaultPropertyDescriptor(attributeName) {
-    const attribute = _characters[attributeName]
+    const attribute = _elements[attributeName]
     return {
       get: function() {
         return attribute[this.index]
       },
       set: function(value) {
-        characters[attributeName](this.index, value)
+        elements[attributeName](this.index, value)
       }
     }
   }
@@ -83,66 +87,66 @@ function Characters(_characters = {
     return {
       get: function() {
         standinArray.length = 0
-        const array = _characters[attributeName][this.index]
+        const array = _elements[attributeName][this.index]
         array.forEach((value, index) => {
           standinArray[index] = value
         })
         return standinArray
       },
       set: function(value) {
-        return characters[attributeName](this.index, value)
+        return elements[attributeName](this.index, value)
       }
     }
   }
 
-  function createSetters(characters) {
-    const attributeNames = Object.keys(_characters)
+  function createSetters(elements) {
+    const attributeNames = Object.keys(_elements)
     attributeNames.forEach(attributeName => {
-      const attribute = _characters[attributeName]
+      const attribute = _elements[attributeName]
       const defaultValue = attribute[0]
       if (Array.isArray(defaultValue)) createArraySetter(attributeName)
       else if (Number.isInteger(defaultValue)) createIntegerSetter(attributeName)
       else if (typeof defaultValue !== 'object') createDefaultSetter(attributeName)
-      else throw console.log('Object or null found in default character')
+      else throw console.log('Object or null found in default element')
     })
   }
 
   function createDefaultSetter(attributeName) {
-    const attribute = _characters[attributeName]
+    const attribute = _elements[attributeName]
     const typeofDefaultValue = typeof attribute[0]
-    characters[attributeName] = function(index, value) {
+    elements[attributeName] = function(index, value) {
       const typofValue = typeof value
       if (typofValue === typeofDefaultValue) attribute[index] = value
-      else throw console.log('characters[' + index + '].' + attributeName + ' must be a ' + typeofDefaultValue)
+      else throw console.log('elements[' + index + '].' + attributeName + ' must be a ' + typeofDefaultValue)
     }
   }
 
   function createIntegerSetter(attributeName) {
-    const attribute = _characters[attributeName]
-    characters[attributeName] = function(index, value) {
+    const attribute = _elements[attributeName]
+    elements[attributeName] = function(index, value) {
       if (Number.isInteger(value)) attribute[index] = value
-      else throw console.log('characters[' + index + '].' + attributeName + ' must be an integer')
+      else throw console.log('elements[' + index + '].' + attributeName + ' must be an integer')
     }
   }
 
   function createArraySetter(attributeName) {
-    const attribute = _characters[attributeName]
+    const attribute = _elements[attributeName]
     let defaultValue = attribute[0]
     defaultValue = defaultValue[0]
     if (Number.isInteger(defaultValue)) createArrayIntegerSetter(attributeName)
-    else throw console.log('Non-integer found in default _characters.' + attributeName)
+    else throw console.log('Non-integer found in default _elements.' + attributeName)
   }
 
   function createArrayIntegerSetter(attributeName) {
-    const attribute = _characters[attributeName]
-    characters[attributeName] = function(index, value) {
+    const attribute = _elements[attributeName]
+    elements[attributeName] = function(index, value) {
       const array = attribute[index]
       if (Number.isInteger(value)) {
         if (value > 0) return push(value, array)
         else if (value < 0) return remove(value, array)
-        else throw console.log('characters[' + index + '].' + attributeName + ' cannot be 0')
+        else throw console.log('elements[' + index + '].' + attributeName + ' cannot be 0')
       }
-      else throw console.log('characters[' + index + '].' + attributeName + ' must be an integer')
+      else throw console.log('elements[' + index + '].' + attributeName + ' must be an integer')
     }
   }
 
@@ -158,21 +162,23 @@ function Characters(_characters = {
     })
     if (match) array[match] = 0
     else {
-      console.log('Could not remove ' + value + ' from characters index attribute. Item not found.')
+      console.log('Could not remove ' + value + ' from elements index attribute. Item not found.')
       return 'no match'
     }
   }
 
-  const characters = {
+  const elements = {
+
+    length: _elements.length,
 
     create: name => createAccessor(createCharacter(name))
 
   }
 
-  const accessorPrototype = createAccessorPrototype(_characters)
-  createSetters(characters)
+  const accessorPrototype = createAccessorPrototype(_elements)
+  createSetters(elements)
 
-  return Object.freeze(Object.create(characters))
+  return Object.freeze(Object.create(elements))
 }
 
-module.exports = Characters
+module.exports = Elements
