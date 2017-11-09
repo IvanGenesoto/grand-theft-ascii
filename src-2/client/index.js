a00315b519dc21a1aba3b83daeca7d49eaf7c8e0
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2017-11-08 19:11:04 -0800

    Removed new version's files. #61

--- src-2/client/index.js
+++
@@ -1,6 +0,0 @@
-const $ = require
-const socket = $('socket.io-client')()
-
-socket.on('connected', message => {
-  console.log(message)
-})
--- src-2/server/initialize/credentials.js
+++
@@ -1,1 +0,0 @@
-module.exports = credentials = '8jelLkefsMk3hjsSf39' // eslint-disable-line no-undef
