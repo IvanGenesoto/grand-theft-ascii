9af6a1dfac6efc3e2f69cc15fc137a6b69e70b11
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2020-06-08 14:33:11 -0700

    Latency compensation accounts for interpolation. #84

--- src/server/get/nearest.js
+++ src/server/get/nearest.js
@@ -1,9 +1,10 @@
-export const getNearest = (reducingKit, vehicleKit) => {
+export const getNearest = (reducingKit, vehicleKit, index) => {
   const {timestamp, smallest = Infinity} = reducingKit
   const {timestamp: timestamp_} = vehicleKit
   const difference = Math.abs(timestamp - timestamp_)
   const isSmallest = difference < smallest
+  const interpolationOffset = 3
   isSmallest && (reducingKit.smallest = difference)
-  isSmallest && (reducingKit.vehicleKit = vehicleKit)
+  isSmallest && (reducingKit.index = index - interpolationOffset)
   return reducingKit
 }
