d85dc0d56e76debfdab6dac89a9fbd938cfa06ba
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2020-07-24 01:37:54 -0700

    pipe.js and descend-vehicle.js are unit tested #92

---
+++ server/do/descend-vehicle.test.js
@@ -0,0 +1,24 @@
+/* global describe it context */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const {expect} = require('chai')
+const {descendVehicle} = require('./descend-vehicle')
+
+describe('descendVehicle', () => {
+  context('if the passed vehicle\'s y is less than 7839', () => {
+    it('increments its y by 5', () => {
+      expect(descendVehicle({y: 0})).to.include({y: 5})
+      expect(descendVehicle({y: 7838})).to.include({y: 7843})
+    })
+  })
+  context('if the passed vehicle\'s y is greater than 7838', () => {
+    it('sets its y to 7843', () => {
+      expect(descendVehicle({y: 7839})).to.include({y: 7843})
+      expect(descendVehicle({y: 8000})).to.include({y: 7843})
+    })
+    it('sets its "isDescending" property to false', () => {
+      expect(descendVehicle({y: 7839})).to.include({y: 7843})
+      expect(descendVehicle({y: 8000})).to.include({y: 7843})
+    })
+  })
+})
